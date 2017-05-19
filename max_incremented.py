b7c5fbc6085235d0b2a89af1b1531e430ce73347
Khare Simran <skhare@hsr.ch>
2017-05-19 21:26:22 +0200

    Extend the jitter_line() function for other geometries as well

--- max_incremented.py
+++ max_incremented.py
@@ -1,9 +1,20 @@
-from qgis.core import *
-from qgis.gui import *
+from qgis.utils import *
 
 @qgsfunction(args='auto', group='Custom')
 def max_incremented(field_name, feature, parent):
-	""" Generates a new value (1 + maximum value) for the given field """
+	"""
+		Auto-generates a new value (1 + maximum value) for the given field 
+		
+		<p><h4>Syntax</h4>
+		max_incremented(<i>field_name</i>)</p>
+
+		<p><h4>Arguments</h4>
+		<i>  field_name</i> &rarr; name of the field for which new values are to be generated<br></p>
+		
+		<p><h4>Example</h4>
+		<!-- Show example of function.-->
+			 max_incremented('id') &rarr; 7</p>
+	"""  
 	layer = iface.activeLayer()
 	field_name_index = layer.fieldNameIndex(field_name)
 	return layer.maximumValue(field_name_index) + 1
